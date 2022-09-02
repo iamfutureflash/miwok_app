af210eedf987ddd0a513640e2a24ce547406e5bd
iamfutureflash <xenode2000@gmail.com>
2022-09-02 16:06:45 +0530

    add array in NumberActivity.java

--- app/src/main/java/com/example/android/miwok/NumberActivity.java
+++ app/src/main/java/com/example/android/miwok/NumberActivity.java
@@ -3,6 +3,7 @@ package com.example.android.miwok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class NumberActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class NumberActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_number);
+        String[] words = {"one","two","three","four","five","six","seven","eight","nine","ten"};
+        for(int i=0;i<words.length;i++) {
+            Log.i("NumbersActivity","word at index : " + i + " " + words[i]);
+        }
     }
 }
